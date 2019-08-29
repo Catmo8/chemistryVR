4046e02552e27aced115018e6dab2b58f28727ad
Catmo8 <catmo8@cox.net>
2019-08-29 01:17:31 -0500

    Added OVR player controller
    It is way more efficient then the player rig I made for the sword game, which is better for oculus quest.

---
+++ Chemistry VR Project/Assets/OculusInputs/Scripts/VrHands.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VrHands : MonoBehaviour
+{
+
+    public GameObject collidingObject;
+    public GameObject heldObject;
+
+    public Transform bothHeldCenter;
+    public Transform otherHand;
+    public static Transform firstHand;
+    public static Transform secondHand;
+
+    static int handCount;
+
+    public Animator anim;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "TwoHandInteractable" || other.tag == "OneHandInteractable")
+        {
+            collidingObject = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == collidingObject)
+        {
+            collidingObject = null;
+        }
+        if(handCount == 2 && other.gameObject == bothHeldCenter.GetChild(0))
+        {
+            anim.SetInteger("Pose", 0);
+            handCount--;
+            bothHeldCenter.GetChild(0).SetParent(otherHand);
+            firstHand = otherHand;
+        }
+    }
+    #region inputMapping
+    public bool isLeftHand;
+    private string grip;
+    private bool gripHeld;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLeftHand)
+        {
+            grip = "Left Grip";
+        }
+        else
+            grip = "Right Grip";
+    }
+    #endregion
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetFloat("Flex", Input.GetAxis(grip));
+
+        if(Input.GetAxis(grip) > 0.5f && !gripHeld)
+        {
+            gripHeld = true;
+            if (collidingObject)
+                Grab();
+        }
+        else if (Input.GetAxis(grip) < 0.5f && gripHeld)
+        {
+            gripHeld = false;
+            if (transform.childCount == 2)
+                Release();
+            else if (handCount == 2)
+            {
+                anim.SetInteger("Pose", 0);
+                handCount--;
+                bothHeldCenter.GetChild(0).SetParent(otherHand);
+                firstHand = otherHand;
+            }
+        }
+        
+        if (handCount == 2)
+        {
+            bothHeldCenter.position = firstHand.position;
+            bothHeldCenter.LookAt(secondHand);
+        }
+    }
+
+    void Grab()
+    {
+        anim.SetInteger("Pose", 1);
+        handCount++;
+        if (collidingObject.tag == "OneHandInteractable")
+            handCount = 1;
+        else if (handCount == 2)
+        {
+            secondHand = transform;
+            bothHeldCenter.position = firstHand.position;
+            bothHeldCenter.LookAt(secondHand);
+            collidingObject.transform.SetParent(bothHeldCenter);
+            return;
+        }
+        firstHand = transform;
+        collidingObject.transform.SetParent(transform);
+        otherHand.GetComponentInChildren<Animator>().SetInteger("Pose", 0);
+    }
+
+    void Release()
+    {
+        anim.SetInteger("Pose", 0);
+        transform.GetChild(1).SetParent(null);
+        handCount--;
+        firstHand = null;
+    }
+}
---
+++ Chemistry VR Project/Assets/OculusInputs/Scripts/VrPlayer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VrPlayer : MonoBehaviour
+{
+    public Transform cameraController;
+    CharacterController charController;
+
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        charController.height = cameraController.position.y;
+    }
+
+    /*
+    public string horizontalInputName;
+    public string verticalInputName;
+    public bool snapTurn; // to be implemented
+    public int snapAngle; // to be implemented
+
+    public float movementSpeed = 2.0f;
+    public float gravity = 20.0f;
+
+    private CharacterController charController;
+    private Vector3 moveDirection;
+    void Start()
+    {
+        charController = GetComponent<CharacterController>();
+        horizontalInputName = "Left Horizontal";
+        verticalInputName = "Left Vertical";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // movement for when Player is grounded
+        if (charController.isGrounded)
+        {
+            // getting movement values
+            moveDirection = new Vector3(Input.GetAxis(horizontalInputName), 0, -Input.GetAxis(verticalInputName));
+            Debug.Log(Input.GetAxis(verticalInputName));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= movementSpeed;
+        }
+
+        moveDirection.y -= (gravity * Time.deltaTime);
+
+        charController.Move(moveDirection * Time.deltaTime);
+    }
+    */
+}
